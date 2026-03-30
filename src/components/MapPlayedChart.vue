200e9809bfa7319318cd419ac8ee5664f0d1a78f
Stéphane Bisinger <stephane@sbisinger.ch>
2026-03-30 20:22:49 +0200

    Add missing maps played chart

---
+++ src/components/MapPlayedChart.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import BarChart, { type SeriesParam } from "./BarChart.vue";
+
+export type GameStats = {
+  civs: Record<
+    string,
+    { wins: number; losses: number; total: number; winrate: number }
+  >;
+  maps: Record<string, number>;
+};
+const props = defineProps<{ games: GameStats }>();
+const played = computed(() =>
+  Object.entries(props.games.maps).toSorted(([_mapA, a], [_mapB, b]) => a - b),
+);
+
+const series: SeriesParam[] = [{ type: "mapPlayed" }];
+</script>
+<template>
+  <div>
+    <BarChart
+      title="Maps Played"
+      yAxis="Games"
+      :counts="played"
+      :series="series"
+    />
+  </div>
+</template>
